84398513bf63e46dcbc7be8bca4ce2acc475d9c4
menachem <AM0556727820@GMAIL.COM>
2024-09-12 09:39:28 +0300

    first commit

---
+++ exam.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Mission1 = (arry) => {
+    return arry.filter(x => x % 2 == 0);
+}
+const myArry = [1, 2, 3, 4, 5, 6]
+console.log(Mission1(myArry));
+
+
+
+
+
+
+
+
+
+
+    
+module.exports = {
+
+// mission1,
+
+// Mission3,
+
+// Mission5,
+
+// Mission6
+}
