3a861b8fc663a85470306ad87266fc3c76e5ddec
menachem <AM0556727820@GMAIL.COM>
2024-09-12 09:56:50 +0300

    סיום

--- exam.js
+++ exam.js
@@ -22,16 +22,37 @@
 
 
 
-const Mission1 = (arry) => {
+const mission1 = (arry) => {
     return arry.filter(x => x % 2 == 0);
 }
 const myArry = [1, 2, 3, 4, 5, 6]
-console.log(Mission1(myArry));
+// console.log(Mission1(myArry));
 
 
+const Mission2 = (str) => {
+    const tolist = str.split(" ");
+    return tolist.filter(x => x.length >= 4).length;
+}
+const myStr = "123 1234 12 1234 12 1234"
+// console.log(Mission2(myStr))
+
+const Mission3 = (matrix) => {
+    return matrix.flat();
+}
+const myMatrix = [[1, 2], [3, 4]];
+// console.log(Mission3(myMatrix))
 
 
-
+const Mission5 = (keys, values) => {
+    const myObg = {};
+    for(let i = 0; i < keys.length; i++){
+        myObg[keys[i]] = values[i];
+    }
+    return myObg;
+}
+const keys = ["1", "2", "3"];
+const values = ["a", "b", "c"];
+// console.log(Mission5(keys, values));
 
 
 
@@ -40,11 +61,11 @@ console.log(Mission1(myArry));
     
 module.exports = {
 
-// mission1,
+mission1,
 
-// Mission3,
+Mission2,
 
-// Mission5,
+Mission3,
 
-// Mission6
+Mission5
 }
